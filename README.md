059aed9298fc58a4042e2824cc51f766f53aae49
daniel.guo <daniel.guo@vivalabs.com.au>
2024-01-07 21:36:36 +1100

    add plugins: Lazy and neo-tree

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# neovim
-My neovim config
+My configs for NeoVim.
+
+# Plugins
+
+## Lazy
+
+- [Lazy](https://github.com/folke/lazy.nvim): the plugin manager
+
+## neo-tree
+
+- [neo-tree](https://github.com/nvim-neo-tree/neo-tree.nvim)
+
+If icons are not shown, please install nerd-fonts, then enable the fonts in your terminal.
+
+```bash
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
+```
+
+Then open `Preferences` in your terminal (iTerm2), select `Hack Nerd Font Mono` in your profile (under **Text** -> **Font**).
+![Set font in iTerm2](./images/iterm-set-font.png)
+
+- [Icons are not showing in nvim-tree](https://vi.stackexchange.com/questions/40279/icons-are-not-showing-in-nvim-tree)
+- [nerd-fonts](https://github.com/ryanoasis/nerd-fonts?tab=readme-ov-file#option-4-homebrew-fonts)
+- [How to change Neovim font?](https://stackoverflow.com/questions/35285300/how-to-change-neovim-font)
---
+++ lua/plugins/configs/neo-tree.lua
@@ -0,0 +1,187 @@
+local status_ok, neo_tree = pcall(require, "neo-tree")
+
+if not status_ok then
+  vim.notify("Neotree not found")
+  return
+end
+
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+neo_tree.setup({
+  close_if_last_window = false,
+  enable_diagnostics = true,
+  enable_git_status = true,
+  source_selector = {
+    winbar = true,
+    content_layout = "center",
+    tab_labels = {
+      filesystem = " File",
+      buffers = "➜ Buffs",
+      git_status = " Git",
+      diagnostics = "",
+    },
+  },
+  default_component_configs = {
+    indent = {
+      indent_size = 2,
+      padding = 1,
+      expander_collapsed = "",
+      expander_expanded = "",
+      expander_highlight = "NeoTreeExpander",
+    },
+    icon = {
+      folder_closed = "",
+      folder_open = "",
+      folder_empty = "",
+      highlight = "NeoTreeFileIcon"
+    },
+    modified = {
+      symbol = "[+]",
+      highlight = "NeoTreeModified",
+    },
+    name = {
+      trailing_slash = false,
+      use_git_status_colors = true,
+      highlight = "NeoTreeFileName",
+    },
+    git_status = {
+      symbols = {
+        unstaged = "✗",
+        staged = "✓",
+        unmerged = "",
+        renamed = "➜",
+        untracked = "★",
+        deleted = "",
+        ignored = "◌",
+      },
+    },
+  },
+  window = {
+    position = "left",
+    width = 40,
+    mappings = {
+      ["<space>"] = {
+        "toggle_node",
+        nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+      },
+      ["<2-LeftMouse>"] = "open",
+      ["<cr>"] = "open",
+      ["<esc>"] = "cancel", -- close preview or floating neo-tree window
+      ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+      -- Read `# Preview Mode` for more information
+      ["l"] = "focus_preview",
+      ["S"] = "open_split",
+      ["s"] = "open_vsplit",
+      -- ["S"] = "split_with_window_picker",
+      -- ["s"] = "vsplit_with_window_picker",
+      ["t"] = "open_tabnew",
+      -- ["<cr>"] = "open_drop",
+      -- ["t"] = "open_tab_drop",
+      ["w"] = "open_with_window_picker",
+      --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+      ["C"] = "close_node",
+      -- ['C'] = 'close_all_subnodes',
+      ["z"] = "close_all_nodes",
+      --["Z"] = "expand_all_nodes",
+      ["a"] = {
+        "add",
+        -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+        -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+        config = {
+          show_path = "none" -- "none", "relative", "absolute"
+        }
+      },
+      ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+      ["d"] = "delete",
+      ["r"] = "rename",
+      ["y"] = "copy_to_clipboard",
+      ["x"] = "cut_to_clipboard",
+      ["p"] = "paste_from_clipboard",
+      ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+      -- ["c"] = {
+      --  "copy",
+      --  config = {
+      --    show_path = "none" -- "none", "relative", "absolute"
+      --  }
+      --}
+      ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+      ["q"] = "close_window",
+      ["R"] = "refresh",
+      ["?"] = "show_help",
+      ["<"] = "prev_source",
+      [">"] = "next_source",
+      ["i"] = "show_file_details",
+    },
+  },
+  filesystem = {
+    follow_current_file = true,
+    hijack_netrw_behavior = "open_current",
+    use_libuv_file_watcher = true,
+    filtered_items = {
+      visible = false,
+      hide_dotfiles = true,
+      hide_gitignored = true,
+      always_show = {
+        ".gitignored",
+      },
+      never_show = {
+        ".DS_Store",
+        ".idea"
+      }
+    },
+  },
+  buffers = {
+    follow_current_file = {
+      enabled = true,          -- This will find and focus the file in the active buffer every time
+      --              -- the current file is changed while the tree is open.
+      leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+    },
+    group_empty_dirs = true,   -- when true, empty folders will be grouped together
+    show_unloaded = true,
+    window = {
+      mappings = {
+        ["bd"] = "buffer_delete",
+        ["<bs>"] = "navigate_up",
+        ["."] = "set_root",
+        ["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+        ["oc"] = { "order_by_created", nowait = false },
+        ["od"] = { "order_by_diagnostics", nowait = false },
+        ["om"] = { "order_by_modified", nowait = false },
+        ["on"] = { "order_by_name", nowait = false },
+        ["os"] = { "order_by_size", nowait = false },
+        ["ot"] = { "order_by_type", nowait = false },
+      }
+    },
+  },
+  git_status = {
+    window = {
+      position = "float",
+      mappings = {
+        ["A"]  = "git_add_all",
+        ["gu"] = "git_unstage_file",
+        ["ga"] = "git_add_file",
+        ["gr"] = "git_revert_file",
+        ["gc"] = "git_commit",
+        ["gp"] = "git_push",
+        ["gg"] = "git_commit_and_push",
+        ["o"]  = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+        ["oc"] = { "order_by_created", nowait = false },
+        ["od"] = { "order_by_diagnostics", nowait = false },
+        ["om"] = { "order_by_modified", nowait = false },
+        ["on"] = { "order_by_name", nowait = false },
+        ["os"] = { "order_by_size", nowait = false },
+        ["ot"] = { "order_by_type", nowait = false },
+      }
+    }
+  },
+
+  event_handlers = {
+    {
+      event = "neo_tree_buffer_enter",
+      handler = function(_)
+        vim.opt_local.signcolumn = "auto"
+      end,
+    },
+  },
+
+})
